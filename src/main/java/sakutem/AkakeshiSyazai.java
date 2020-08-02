1cd61c25ff5b1e0fe38ad10e660241007364108f
sakusakutempra <garagara324298@gmail.com>
2020-08-02 19:44:25 +0900

    できた

--- src/main/java/sakutem/AkakeshiSyazai.java
+++ src/main/java/sakutem/AkakeshiSyazai.java
@@ -1,19 +1,13 @@
 package sakutem;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.TntEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.world.World;
-
-import java.util.UUID;
 
 
 public class AkakeshiSyazai implements ModInitializer {
@@ -22,95 +16,5 @@ public class AkakeshiSyazai implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-/*
-		// ヒツジの毛をダイアにするやつ.
-		SyazaiCallback.EVENT.register((player, sheep) -> {
-			sheep.setSheared(true);
-
-			// create diamond item entity at sheep position
-			ItemStack stack = new ItemStack(Items.DIAMOND);
-			ItemEntity itemEntity = new ItemEntity(
-					player.world,
-					sheep.getX(),
-					sheep.getY(),
-					sheep.getZ(),
-					stack
-			);
-			player.world.spawnEntity(itemEntity);
-
-			return ActionResult.FAIL;
-		});
-
- */
-		SyazaiCallback.EVENT.register((string, player_entity) -> {
-//			System.out.println("log_output");
-//			System.out.println(string);
-
-			World world = player_entity.world;
-
-			ItemStack stack = new ItemStack(Items.DIAMOND);
-			ItemEntity itemEntity = new ItemEntity(
-					player_entity.world,
-					player_entity.getX(),
-					player_entity.getY() + 5.0f,
-					player_entity.getZ(),
-					stack
-			);
-			player_entity.world.spawnEntity(itemEntity);
-
-			ItemEntity itemEntity2 = new ItemEntity(
-					world,
-					100.0f,
-					70.0f,
-					105.0f,
-					stack
-			);
-			world.spawnEntity(itemEntity2);
-			TntEntity tnt_entity = EntityType.TNT.create(world);
-			tnt_entity.setFuse(0);
-			tnt_entity.setPos(
-					player_entity.getX(),
-					player_entity.getY(),
-					player_entity.getZ()
-			);
-			world.spawnEntity(tnt_entity);
-
-//			player_entity.setPos(100.0f, 100.0f, 100.0f);
-//			player_entity.setAir(1);
-
-/*			System.out.println("world " + world.getDebugString());
-			System.out.println("world dimention " + player_entity.world.getDimension().toString());
-			System.out.println("world dimention canPlayerSleep " + player_entity.world.getDimension().canPlayersSleep());
-			System.out.println("world dimention nether " + player_entity.world.getDimension().isNether());
-			System.out.println("world difficulty " + player_entity.world.getDifficulty().toString());
-			System.out.println("world instanceof " + (player_entity.world instanceof World));
-
-			System.out.println("Dare " + player_entity.getName().getString());	// これで名前とれる.
-			System.out.println("Dare " + player_entity.getName().asString());	// これでもとれる.
-			System.out.println("Dare " + player_entity.getName().toString());	// 情報を全部文字列として出してる感じ.
-*/
-
-
-//			UUID player_uuid = player_entity.getUuid();
-
-//			MinecraftServer server = player_entity.getServer();
-//			System.out.println("server " + server.getUserName());
-//			ServerPlayerEntity server_player_entity = server.getPlayerManager().getPlayer(player_uuid);
-//			System.out.println("server_sendChatMessage : " + server_player_entity.world.getDebugString() );
-
-			/*			ItemEntity itemEntity = new ItemEntity(
-					server_player_entity.world,
-					player_entity.getX() + 5.0,
-					player_entity.getY() + 5.0,
-					player_entity.getZ(),
-					stack
-			);
-			server_player_entity.world.spawnEntity(itemEntity);
-			*/
-
-
-			return ActionResult.SUCCESS; // キャンセルすると、そこで止まる.キャンセルしないと、同一の条件のリスナも実行される（superの処理を阻害しない）.
-		});
 	}
 }
--- src/main/java/sakutem/mixin/SakutemMixin.java
+++ src/main/java/sakutem/mixin/SakutemMixin.java
@@ -1,41 +1,25 @@
 package sakutem.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.TntEntity;
-import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.integrated.IntegratedServer;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import sakutem.SyazaiCallback;
 
 import java.util.Random;
 import java.util.UUID;
 
-//@Mixin(TitleScreen.class)
-//@Mixin(SheepEntity.class)
-//@Mixin(PlayerEntity.class)
 @Mixin(ClientPlayerEntity.class)
-//@Mixin(Entity.class)
 public class SakutemMixin {
 
 	@Shadow @Final protected MinecraftClient client;
@@ -55,105 +39,41 @@ public class SakutemMixin {
 		ClientPlayerEntity client_player_entity = client.player;
 		PlayerEntity player_entity = (PlayerEntity)client_player_entity;
 
-
-		ActionResult result = SyazaiCallback.EVENT.invoker().interact(string, player_entity);
-
-
-
-
-		if(string.equals("test1")) {
-			System.out.println("test1 start");
-			IntegratedServer integrated_server = client.getServer();
-			System.out.println(integrated_server.toString());
-			System.out.println("test1 end");
-		}
-		if(string.equals("test2")) {
-			System.out.println("test2 start");
-			IntegratedServer integrated_server = client.getServer();
-			System.out.println(integrated_server.toString());
-			PlayerManager player_manager = integrated_server.getPlayerManager();
-			System.out.println(player_manager.toString());
-			System.out.println("test2 end");
-		}
-		if(string.equals("test3")){
-			System.out.println("test3 start");
-			IntegratedServer integrated_server = client.getServer();
-			System.out.println(integrated_server.toString());
-			PlayerManager player_manager = integrated_server.getPlayerManager();
-			System.out.println(player_manager.toString());
-			UUID player_uuid = player_entity.getUuid();
-			ServerPlayerEntity server_player = player_manager.getPlayer(player_uuid);
-			System.out.println(server_player.getName());
-			System.out.println("test3 end");
-		}
-		if(string.equals("test4")){
-			System.out.println("test4 start");
-			IntegratedServer integrated_server = client.getServer();
-			System.out.println(integrated_server.toString());
-			PlayerManager player_manager = integrated_server.getPlayerManager();
-			System.out.println(player_manager.toString());
-			UUID player_uuid = player_entity.getUuid();
-			ServerPlayerEntity server_player_entity = player_manager.getPlayer(player_uuid);
-			System.out.println(server_player_entity.getName());
-
-			ItemStack stack = new ItemStack(Items.DIAMOND);
-			ItemEntity itemEntity = new ItemEntity(
-					server_player_entity.world,
-					server_player_entity.getX() + 5.0,
-					server_player_entity.getY() + 5.0,
-					server_player_entity.getZ() + 5.0,
-					stack
-			);
-			server_player_entity.world.spawnEntity(itemEntity);
-
-			System.out.println("test4 end");
-		}
+		String[] strKeyword = new String[KEYWORD_NUM];
+		strKeyword[0] = "ごめんなさい";
+		strKeyword[1] = "アカウント消";
+		strKeyword[2] = "アカ消";
+		strKeyword[3] = "もうマイクラしません";
+		strKeyword[4] = "もうこの界隈離れます";
+		strKeyword[5] = "無理です";
+		strKeyword[6] = "くんさんにであえてよかった";
+		strKeyword[7] = "gomi";
+		strKeyword[8] = "50人クラフト辞め";
+		strKeyword[9] = "今まですみませんでした";
+		strKeyword[10] = "謝罪配信";
+		strKeyword[11] = "スパチャもういい";
 
 		int syazai_score = 0;
 
-		if( string.contains("ごめんなさい") ){
-			syazai_score++;
-			System.out.println("gomennasai");
-		}
-
-		if( string.contains("アカウント消します") ){
-			syazai_score++;
-			System.out.println("akauntokesimasu");
-		}
-
-		if( string.contains("病") ){
-			syazai_score++;
-			System.out.println("yamu");
-		}
-
-		if( string.contains("もう無理") ){
-			syazai_score++;
-			System.out.println("moumuri");
-		}
-
-		if( string.contains("アカ消しします") ){
-			syazai_score++;
-			System.out.println("akakesisimasu");
-		}
-		if( string.contains("suman") ){
-			syazai_score++;
-			System.out.println("suman");
+		for(int i = 0; i < KEYWORD_NUM; i++){
+			if( string.contains(strKeyword[i]) ){
+				syazai_score++;
+				System.out.println("syazai_score " + syazai_score + " hit " + strKeyword[i]);
+			}
 		}
 
 		if(syazai_score > 0){
+			// サーバーWorldの取得.
 			IntegratedServer integrated_server = client.getServer();
 			PlayerManager player_manager = integrated_server.getPlayerManager();
 			UUID player_uuid = player_entity.getUuid();
 			ServerPlayerEntity server_player_entity = player_manager.getPlayer(player_uuid);
-			System.out.println("get treasure:" + syazai_score + " who:" + server_player_entity.getName());
-
 
 			ItemStack stack_diamond = new ItemStack(Items.DIAMOND, syazai_score);
 			ItemStack stack_gold = new ItemStack(Items.GOLD_INGOT, syazai_score);
 			Random random = new Random();
 			float sinXZ, cosXZ, rate, dist = 3.0f;
-			int treasure_num = (int)Math.pow(2.0, (double)syazai_score);
-			treasure_num = syazai_score;
+			int treasure_num = syazai_score;
 			for(int i = 0; i < treasure_num; i++ ){
 				rate = random.nextFloat();
 				sinXZ = (float)Math.sin( Math.PI * 2 * rate ) * dist;
@@ -167,46 +87,9 @@ public class SakutemMixin {
 						(i%2 == 0) ? stack_diamond : stack_gold
 				);
 				server_player_entity.world.spawnEntity(itemEntity);
-
-				System.out.println("spawn x " + sinXZ + " z " + cosXZ + " rate " + rate + " dist " + (sinXZ*sinXZ + cosXZ*cosXZ) );
 			}
 		}
-
-
-
-		if(result == ActionResult.SUCCESS){
-			ItemStack stack = new ItemStack(Items.DIAMOND);
-/*			ItemEntity itemEntity = new ItemEntity(
-					player_entity.world,
-					player_entity.getX() + 5.0,
-					player_entity.getY() + 5.0,
-					player_entity.getZ() + 5.0,
-					stack
-			);
-			player_entity.world.spawnEntity(itemEntity);*/
-
-			ItemEntity itemEntity2 = new ItemEntity(
-					player_entity.world,
-					player_entity.getX(),
-					player_entity.getY(),
-					player_entity.getZ(),
-					stack
-			);
-			player_entity.world.spawnEntity(itemEntity2);
-
-			TntEntity tnt_entity = EntityType.TNT.create(player_entity.world);
-			tnt_entity.setFuse(0);
-			tnt_entity.setPos(
-					player_entity.getX(),
-					player_entity.getY(),
-					player_entity.getZ()
-			);
-			player_entity.world.spawnEntity(tnt_entity);
-
-		}
-
-		if(result == ActionResult.FAIL) {
-			info.cancel();
-		}
 	}
+
+	final int KEYWORD_NUM = 12;
 }
