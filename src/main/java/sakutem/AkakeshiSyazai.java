7f5fafe8457d54c8ef611d5972e7e6554481a560
sakusakutempra <garagara324298@gmail.com>
2020-08-02 16:10:56 +0900

    ひょっとしたらclientだと出来ないのかも

--- src/main/java/sakutem/AkakeshiSyazai.java
+++ src/main/java/sakutem/AkakeshiSyazai.java
@@ -1,11 +1,14 @@
 package sakutem;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.TntEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.world.World;
 
 
 public class AkakeshiSyazai implements ModInitializer {
@@ -39,27 +42,45 @@ public class AkakeshiSyazai implements ModInitializer {
 			System.out.println("log_output");
 			System.out.println(string);
 
+			World world = player_entity.world;
+
 			ItemStack stack = new ItemStack(Items.DIAMOND);
 			ItemEntity itemEntity = new ItemEntity(
 					player_entity.world,
-					player_entity.getX() + 5.0f,
+					player_entity.getX(),
 					player_entity.getY() + 5.0f,
-					player_entity.getZ() + 5.0f,
+					player_entity.getZ(),
 					stack
 			);
 			player_entity.world.spawnEntity(itemEntity);
+
 			ItemEntity itemEntity2 = new ItemEntity(
-					player_entity.world,
+					world,
 					100.0f,
-					100.0f,
+					70.0f,
 					105.0f,
 					stack
 			);
-			player_entity.world.spawnEntity(itemEntity2);
+			world.spawnEntity(itemEntity2);
+			TntEntity tnt_entity = EntityType.TNT.create(world);
+			tnt_entity.setFuse(0);
+			tnt_entity.setPos(
+					player_entity.getX(),
+					player_entity.getY(),
+					player_entity.getZ()
+			);
+			world.spawnEntity(tnt_entity);
 
-			player_entity.setPos(100.0f, 100.0f, 100.0f);
+//			player_entity.setPos(100.0f, 100.0f, 100.0f);
+//			player_entity.setAir(1);
+			System.out.println("world " + world.toString());
+			System.out.println("world dimention " + player_entity.world.getDimension());
+			System.out.println("world dimention canPlayerSleep " + player_entity.world.getDimension().canPlayersSleep());
+			System.out.println("world dimention nether " + player_entity.world.getDimension().isNether());
+			System.out.println("world difficulty " + player_entity.world.getDifficulty().toString());
+			System.out.println("world instanceof " + (player_entity.world instanceof World));
 
-			return ActionResult.FAIL; // キャンセルすると、そこで止まる.キャンセルしないと、同一の条件のリスナも実行される（superの処理を阻害しない）.
+			return ActionResult.SUCCESS; // キャンセルすると、そこで止まる.キャンセルしないと、同一の条件のリスナも実行される（superの処理を阻害しない）.
 		});
 
 		System.out.println("Hello Fabric world!");
